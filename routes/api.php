6fb07da78123bd4786fda1277e1a3f11ccf4fa01
elshodatc111 <elshodatc1116@gmail.com>
2024-09-12 21:53:02 +0500

    Save

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('user/login', [ApiUserController::class, 'login']);
-Route::post('user/login/code', [ApiUserController::class, 'code']);
-Route::post('user/login/name', [ApiUserController::class, 'nameCreate']);
+Route::post('user/login', [ApiUserController::class, 'login']); // Telefon raqamni so'rash uchun
+Route::post('user/login/code', [ApiUserController::class, 'code']);  //  Tasdiqlash kodini tekshiradi va profelga kiradi birinchi marta ro'yhatdan o'tqan bo'lsa Ismini so'raydi
+Route::post('user/login/name', [ApiUserController::class, 'nameCreate']);   // Yangi buyurtmachi uchun ismini so'raladi
 
 
 
 Route::group(['middleware'=>['auth:sanctum']], function(){
-    Route::post('user/home', [ApiUserController::class, 'home']);
-    Route::get('user/home/{id}', [ApiUserController::class, 'homeShow']);
-    Route::post('user/buyurtma', [ApiUserController::class, 'buyurtma']);
+    Route::post('user/home', [ApiUserController::class, 'home']); // Bosh sahifa
+    Route::get('user/home/{id}', [ApiUserController::class, 'homeShow']);   // Mahsulot haqida malumot olish uchun
+    Route::post('user/buyurtma', [ApiUserController::class, 'buyurtma']);   // Buyurtma qabul qilish uchun
 });
